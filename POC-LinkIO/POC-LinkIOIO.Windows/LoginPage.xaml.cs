b8856e9d80fc913b4dedbab0145fe0f292e12eac
valent141 <valent141@hotmail.fr>
2016-03-11 17:41:58 +0100

    add login/menu pages

---
+++ POC-LinkIO/POC-LinkIOIO.Windows/LoginPage.xaml.cs
@@ -0,0 +1,63 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.IO;
+using System.Linq;
+using System.Runtime.InteropServices.WindowsRuntime;
+using Windows.Foundation;
+using Windows.Foundation.Collections;
+using Windows.UI.Xaml;
+using Windows.UI.Xaml.Controls;
+using Windows.UI.Xaml.Controls.Primitives;
+using Windows.UI.Xaml.Data;
+using Windows.UI.Xaml.Input;
+using Windows.UI.Xaml.Media;
+using Windows.UI.Xaml.Navigation;
+
+namespace POC_LinkIO
+{
+    /// <summary>
+    /// Une page vide peut être utilisée seule ou constituer une page de destination au sein d'un frame.
+    /// </summary>
+    public sealed partial class LoginPage : Page, INotifyPropertyChanged
+    {
+        private string login;
+
+        public LoginPage()
+        {
+            this.InitializeComponent();
+            DataContext = this;
+        }
+
+        public string Login
+        {
+            get
+            {
+                return login;
+            }
+            set
+            {
+                login = value;
+                OnPropertyChanged("Login");
+            }
+        }
+
+        public event PropertyChangedEventHandler PropertyChanged;
+        private void OnPropertyChanged(String name)
+        {
+            if (PropertyChanged != null)
+            {
+                PropertyChanged(this, new PropertyChangedEventArgs(name));
+            }
+        }
+
+        private void Button_Click(object sender, RoutedEventArgs e)
+        {
+            Frame rootFrame = Window.Current.Content as Frame;
+            if (!rootFrame.Navigate(typeof(MenuPage), Login))
+            {
+                throw new Exception("Failed to go to the next page.");
+            }
+        }
+    }
+}
---
+++ POC-LinkIO/POC-LinkIOIO.Windows/MenuPage.xaml.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.IO;
+using System.Linq;
+using System.Runtime.InteropServices.WindowsRuntime;
+using Windows.Foundation;
+using Windows.Foundation.Collections;
+using Windows.UI.Xaml;
+using Windows.UI.Xaml.Controls;
+using Windows.UI.Xaml.Controls.Primitives;
+using Windows.UI.Xaml.Data;
+using Windows.UI.Xaml.Input;
+using Windows.UI.Xaml.Media;
+using Windows.UI.Xaml.Navigation;
+
+namespace POC_LinkIO
+{
+    /// <summary>
+    /// Une page vide peut être utilisée seule ou constituer une page de destination au sein d'un frame.
+    /// </summary>
+    public sealed partial class MenuPage : Page
+    {
+        public MenuPage()
+        {
+            this.InitializeComponent();
+        }
+
+        protected override void OnNavigatedTo(NavigationEventArgs e)
+        {
+            Login = e.Parameter as string;
+        }
+
+        public string Login
+        {
+            get;
+            set;
+        }
+
+        private void Button_Click(object sender, RoutedEventArgs e)
+        {
+            Frame rootFrame = Window.Current.Content as Frame;
+            if (!rootFrame.Navigate(typeof(MainPage), Login))
+            {
+                throw new Exception("Failed to go to the next page.");
+            }
+        }
+    }
+}
